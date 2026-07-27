665c5b1584bec16137e3903a22740e416c106768
masum-k <maasumkamal@gmail.com>
2026-07-28 05:13:03 +0600

    created post aoi

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-import dns from "node:dns/promises";
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
+const dns = require("dns");
+
+dns.setServers([
+    "1.1.1.1",
+    "8.8.8.8"
+]);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const port = 3001
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.MONGO_DB_URI;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,21 +38,23 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const database = client.db("bibliodrop");
+        const bookCollection = database.collection("books");
+
+        app.post('/api/librarians', async (req, res) => {
+            const book = req.body;
+            const result = await bookCollection.insertOne(book);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
